7518c05d23debef0650b2f0a92ad65d615bdab7f
Anindya De <anindya.de@mphasis.com>
2022-08-01 16:23:53 -0500

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Anindya De on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
